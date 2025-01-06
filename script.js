4a153ac6b9893819e3ef0bf16fef5fa4010403fe
SAKIB SAYYAD <sakibsayyad29706@gmail.com>
2025-01-06 23:43:20 +0530

    adding gsap

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+function locoMotiveScroll(){
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('#main'),
+        smooth: true
+    });
+    
+}
+locoMotiveScroll();
+
+gsap.from("#abouthead h1,h3",{
+    y: 100,
+    opacity: 0,
+    delay: 0.5,
+    duration: 0.9,
+    stagger: 0.3,
+});
+
+gsap.from(".about2",{
+    y: 100,
+    opacity: 0,
+    delay: 0.5,
+    duration: 1.1,
+    stagger: 0.3,
+});
+gsap.from(".aboutpara2",{
+    y: 100,
+    opacity: 0,
+    delay: 0.5,
+    duration: 1.1,
+    stagger: 0.3,
 });
